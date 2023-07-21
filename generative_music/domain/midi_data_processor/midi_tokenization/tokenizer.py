ad901d770a3465e610bac736451bde48580ffd32
atsushieee <atsushi.tabata1204@gmail.com>
2023-07-22 08:04:08 +0900

    add: tokenizer class

---
+++ generative_music/domain/midi_data_processor/midi_tokenization/tokenizer.py
@@ -0,0 +1,67 @@
+"""A module to tokenize event2id and detokenize id2event for DNN learning."""
+import json
+
+from generative_music.domain.midi_data_processor.config import Config
+from generative_music.domain.midi_data_processor.data_elements import (
+    Event, EventName)
+
+
+class Tokenizer:
+    """This class is responsible for tokenizing and detokenizing events.
+
+    The Tokenizer maps events to unique integer IDs and vice versa.
+    It allows for easy conversion between events and their corresponding IDs,
+    which is useful for machine learning models that require numerical inputs.
+    """
+
+    def __init__(self, config: Config):
+        """Initialize the tokenizer with token mapping from the given Config instance.
+
+        Args:
+            config (Config):
+                An instance of the Config class containing token mapping file paths.
+        """
+        with open(config.EVENT2ID_FILEPATH, "r") as f:
+            self.event2id = json.load(f)
+
+        with open(config.ID2EVENT_FILEPATH, "r") as f:
+            self.id2event = json.load(f)
+
+    def tokenize(self, event: Event) -> int:
+        """Convert a given event to its corresponding ID.
+
+        Args:
+            event (Event): The event to be tokenized.
+
+        Returns:
+            int: The ID corresponding to the given event.
+
+        Raises:
+            KeyError: If the given event is not found in the event2id mapping.
+        """
+        event_key = f"{event.name.value}_{event.value}"
+        if event_key not in self.event2id:
+            raise KeyError(
+                f"The event '{event_key}' is not found in the event2id mapping."
+            )
+        return self.event2id[event_key]
+
+    def detokenize(self, id: int, time: int) -> Event:
+        """Convert a given ID to its corresponding event.
+
+        Args:
+            id (int): The ID to be detokenized.
+            time (int): starting event time (tick).
+
+        Returns:
+            Event: The event corresponding to the given ID.
+
+        Raises:
+            KeyError: If the given ID is not found in the id2event mapping.
+        """
+        id_str = str(id)
+        if id_str not in self.id2event:
+            raise KeyError(f"The ID '{id}' is not found in the id2event mapping.")
+        event_str = self.id2event[id_str]
+        name, value = event_str.split("_", 1)
+        return Event(name=EventName(name), time=time, value=value)
---
+++ tests/domain/midi_data_processor/midi_tokenization/test_tokenizer.py
@@ -0,0 +1,48 @@
+"""Tests for a class that tokenize event2id and detokenize id2event."""
+from generative_music.domain.midi_data_processor.config import Config
+from generative_music.domain.midi_data_processor.data_elements import (
+    Event, EventName)
+from generative_music.domain.midi_data_processor.midi_tokenization.tokenizer import \
+    Tokenizer
+
+
+class TestTokenizer:
+    """A test class for the Tokenizer.
+
+    The class is responsible for testing the tokenization and detokenization of events.
+    It checks if the events are correctly tokenized and detokenized,
+    and if the results match the expected values.
+    """
+
+    def setup_method(self):
+        """Initialize the Tokenizer tests.
+
+        Set up the test environment by creating test items
+        and initializing the Config instance.
+        This method is called before each test function is executed.
+        """
+        config = Config()
+        self.tokenizer = Tokenizer(config)
+
+    def test_tokenize(self):
+        """Test if the tokenization correctly converts an Event instance to an integer."""
+        event = Event(EventName.POSITION, 0, "1/16")
+        event_id = self.tokenizer.tokenize(event)
+        assert isinstance(event_id, int)
+
+    def test_detokenize(self):
+        """Test if the detokenization correctly converts an integer to an Event instance."""
+        event_id = 42
+        time = 0
+        event = self.tokenizer.detokenize(event_id, time)
+        assert isinstance(event, Event)
+        assert event.time == time
+
+    def test_tokenize_detokenize_consistency(self):
+        """Test if the tokenization and detokenization are consistent."""
+        event = Event(EventName.POSITION, 1920, "1/16")
+        event_id = self.tokenizer.tokenize(event)
+        detokenized_event = self.tokenizer.detokenize(event_id, event.time)
+        assert event.name == detokenized_event.name
+        assert event.time == detokenized_event.time
+        assert event.value == detokenized_event.value
