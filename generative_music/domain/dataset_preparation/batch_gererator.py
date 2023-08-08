d0b89d6cdb6eafd083755958e1ed4ce5ade460bc
atsushieee <atsushi.tabata1204@gmail.com>
2023-08-09 06:06:50 +0900

    add: data batch generator

---
+++ generative_music/domain/dataset_preparation/batch_gererator.py
@@ -0,0 +1,299 @@
+"""A module for generating batches of sequences."""
+from typing import List, Tuple
+
+import tensorflow as tf
+
+
+class BatchGenerator:
+    """A class for generating batches of sequences from a dataset.
+
+    This class creates batches of sequences with specified length,
+    and shuffles and pads the training data as needed.
+    """
+
+    def __init__(
+        self,
+        data: List[List[int]],
+        batch_size: int,
+        seq_length: int,
+        padding_id: int,
+        start_token_id: int,
+        vocab_size: int,
+    ):
+        """Initialize the BatchGenerator instance.
+
+        Args:
+            data (List[List[int]]): The input data as a list of tokenized sequences.
+            batch_size (int): The number of sequences in a batch.
+            seq_length (int): The length of each sequence in a batch.
+            padding_id (int): The token ID used for padding.
+            start_token_id (int): The token ID used to indicate the start of a sequence.
+            vocab_size (int):
+                The size of the number of unique words or tokens in the dataset.
+        """
+        self.data = data
+        self.batch_size = batch_size
+        self.subseq_length = tf.constant(seq_length, dtype=tf.int32)
+        self.padding_id = tf.constant(padding_id, dtype=tf.int32)
+        self.start_token_id = tf.constant(start_token_id, dtype=tf.int32)
+        self.vocab_size = vocab_size
+
+    def generate_batches(self) -> tf.data.Dataset:
+        """Generate batches of sequences from the input data.
+
+        Returns:
+            tf.data.Dataset:
+                The dataset containing the generated batches, behaving like a generator.
+        """
+        dataset = tf.data.Dataset.from_generator(
+            lambda: iter(self.data), output_types=tf.int32, output_shapes=(None,)
+        )
+        # Shuffle the dataset.
+        # Adjust buffer_size based on the actual data size and machine specifications.
+        dataset = dataset.shuffle(buffer_size=len(self.data))
+        # When batching a dataset with variable shapes,
+        # it is necessary to use Dataset.padded_batch.
+        dataset = dataset.padded_batch(self.batch_size, padding_values=self.padding_id)
+        dataset = dataset.map(
+            self._process_sequences,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
+        # Prefetch optimizes the pipeline by overlapping data preprocessing and model training.
+        # AUTOTUNE automatically determines the optimal buffer size, improving training speed.
+        dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+        return dataset
+
+    @tf.function
+    def _process_sequences(self, sequences: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+        """Process sequences to create the source and target sequences.
+
+        This function takes the input sequences and creates two new sequences:
+        the source sequence with all elements except the last one,
+        and the target sequence with all elements except the first one,
+        which is then one-hot encoded.
+        This is useful for language modeling tasks,
+        where the goal is to predict the next token given a sequence of tokens.
+
+        Args:
+            sequences (tf.Tensor):
+                The input sequence as a TensorFlow tensor with shape
+                (batch_size, max_sequence_length).
+
+        Returns:
+            Tuple[tf.Tensor, tf.Tensor]:
+                The source sequences as a TensorFlow tensor
+                with shape (batch_size, max_sequence_length - 1)
+                and the one-hot encoded target sequences as a TensorFlow tensor
+                with shape (batch_size, max_sequence_length - 1, vocab_size).
+        """
+        subsequences = self._extract_subsequences(sequences)
+        src = subsequences[:, :-1]
+        # One-hot encode tgt
+        tgt = subsequences[:, 1:]
+        tgt = tf.one_hot(tgt, depth=self.vocab_size, dtype=tf.int32)
+        return src, tgt
+
+    @tf.function
+    def _extract_subsequences(self, sequences: tf.Tensor) -> tf.Tensor:
+        """Extract subsequences of the given length from the input sequence.
+
+        Args:
+            sequences (tf.Tensor):
+                The input sequence as a TensorFlow tensor with shape
+                (batch_size, max_sequence_length).
+
+        Returns:
+            tf.Tensor: The extracted subsequences as a tensor of token IDs.
+        """
+        # Use tf.map_fn to apply the _extract_subsequence function
+        # to each sequence in the batch.
+        result = tf.map_fn(
+            lambda seq: self._extract_subsequence(seq),
+            sequences,
+            dtype=tf.int32,
+        )
+        return result
+
+    @tf.function
+    def _extract_subsequence(self, sequence: tf.Tensor) -> tf.Tensor:
+        """Extract a subsequence of the given length from the input sequence.
+
+        If the desired subsequence length is longer than the length of the score,
+        use the entire score.
+        If it's shorter, truncate the song at the beginning of measures.
+
+        Args:
+            sequence (tf.Tensor):
+                The input sequence as a TensorFlow tensor of token IDs.
+
+        Returns:
+            tf.Tensor: The extracted a subsequence as a tensor of token IDs.
+        """
+        # Find the indices of start_token_id in the sequence
+        start_indices = tf.where(tf.equal(sequence, self.start_token_id))
+        # Reshape the start_indices tensor to a 1D list
+        start_indices = tf.reshape(start_indices, [-1])
+        # Cast start_indices from tf.int64 to tf.int32
+        start_indices = tf.cast(start_indices, dtype=tf.int32)
+
+        tf.debugging.assert_greater(
+            tf.size(start_indices),
+            0,
+            message="No valid start token found in the sequence.",
+        )
+        # Extract the subsequence from the first start index to the end of the sequence
+        if self.subseq_length >= tf.shape(sequence)[0]:
+            return self._pad_sequence(sequence[start_indices[0] :])
+
+        actual_seq_length = self._find_actual_seq_length(sequence)
+        start_index = self._select_start_index(start_indices, actual_seq_length)
+        end_index = start_index + self.subseq_length
+        # If it's in the middle of a measure, truncate it.
+        no_padding_end_index = self._find_end_index(sequence, start_index)
+        modified_sequence = self._replace_trailing_values_with_padding(
+            sequence, no_padding_end_index
+        )
+        return modified_sequence[start_index:end_index]
+
+    @tf.function
+    def _pad_sequence(self, sequence: tf.Tensor) -> tf.Tensor:
+        """Pad the input sequence to the target length with the padding token.
+
+        Args:
+            sequence (tf.Tensor):
+                The input sequence as a TensorFlow tensor of token IDs.
+
+        Returns:
+            tf.Tensor: The padded sequence as a tensor.
+        """
+        padding = [[self.padding_id, self.subseq_length - tf.shape(sequence)[0]]]
+        padded_sequence = tf.pad(sequence, padding)
+        return padded_sequence
+
+    @tf.function
+    def _find_actual_seq_length(self, sequence: tf.Tensor) -> tf.Tensor:
+        """Find the actual length of the input sequence, considering padding tokens.
+
+        Args:
+            sequence (tf.Tensor):
+                The input sequence as a TensorFlow tensor of token IDs.
+
+        Returns:
+            Tuple[tf.Tensor, tf.Tensor]:
+                A tuple containing the actual sequence length.
+        """
+        # If there are any pad_indices, use the first one as the end of the sequence;
+        # otherwise, use the original sequence length
+        pad_indices = tf.where(tf.equal(sequence, self.padding_id))
+        # Reshape the start_indices tensor to a 1D list
+        pad_indices = tf.reshape(pad_indices, [-1])
+        # Cast start_indices from tf.int64 to tf.int32
+        pad_indices = tf.cast(pad_indices, dtype=tf.int32)
+        actual_seq_length = tf.cond(
+            tf.size(pad_indices) > 0,
+            lambda: pad_indices[0],
+            lambda: tf.shape(sequence)[0],
+        )
+        return actual_seq_length
+
+    @tf.function
+    def _select_start_index(
+        self, start_indices: tf.Tensor, actual_seq_length: tf.Tensor
+    ) -> tf.Tensor:
+        """Select a random start index from the valid start indices.
+
+        Args:
+            start_indices (tf.Tensor):
+                A 1D TensorFlow tensor containing the start indices.
+            actual_seq_length (tf.Tensor):
+                A scalar tensor representing the actual sequence length.
+
+        Returns:
+            tf.Tensor: A scalar tensor representing the selected start index.
+        """
+        valid_start_indices = tf.boolean_mask(
+            start_indices,
+            tf.greater_equal(actual_seq_length - start_indices, self.subseq_length),
+        )
+        if tf.size(valid_start_indices) > 0:
+            start_index = tf.random.shuffle(valid_start_indices)[0]
+        else:
+            start_index = start_indices[0]
+        return start_index
+
+    @tf.function
+    def _find_end_index(self, sequence: tf.Tensor, start_index: tf.Tensor) -> tf.Tensor:
+        """Find the end index based on the given sequence and start index.
+
+        The end index is determined with the following priority:
+        1. If adding seq_length - 1 to the start_index results in an index
+        that is greater than or equal to the last index of the sequence,
+        the end index is set to the last index of the sequence.
+        2. If the element in the sequence at the position (end_index + 1)
+        is equal to the start_token_id, the end index remains unchanged.
+        3. Check if there is any occurrence of start_token_id within the subsequence
+        from (start_index + 1) to (end_index + 1). If not, return the current end_index.
+        4. Otherwise, the end index is set to the last occurrence of start_token_id
+        within the subsequence from (start_index + 1) to (end_index + 1).
+
+        Args:
+            sequence (tf.Tensor): The input sequence as a tensor.
+            start_index (tf.Tensor): The starting index of the subsequence.
+
+        Returns:
+            tf.Tensor: The calculated end index.
+        """
+        end_index = start_index + self.subseq_length - 1
+        if end_index >= tf.shape(sequence)[0] - 1:
+            return tf.shape(sequence)[0] - 1
+        else:
+            next_token_is_start = tf.equal(sequence[end_index + 1], self.start_token_id)
+
+        def use_last_matching_index() -> tf.Tensor:
+            matching_indices = tf.where(
+                tf.equal(sequence[start_index + 1 : end_index + 1], self.start_token_id)
+            )
+            if tf.size(matching_indices) == 0:
+                return end_index
+            last_matching_index = tf.reduce_max(matching_indices)
+            # Cast start_indices from tf.int64 to tf.int32
+            last_matching_index = tf.cast(last_matching_index, dtype=tf.int32)
+            return start_index + last_matching_index
+
+        # Update the end index based on whether the next token is the start token or not,
+        # and if not, use the last matching index of the start token within the subsequence
+        end_index = tf.cond(
+            next_token_is_start, lambda: end_index, use_last_matching_index
+        )
+        return end_index
+
+    @tf.function
+    def _replace_trailing_values_with_padding(
+        self, sequence: tf.Tensor, no_padding_end_index: tf.Tensor
+    ) -> tf.Tensor:
+        """Replace the values in the input sequence after the given index with padding tokens.
+
+        Args:
+            sequence (tf.Tensor):
+                The input sequence as a 1D TensorFlow tensor.
+            no_padding_end_index (tf.Tensor):
+                A scalar tensor representing the index
+                after which the padding should be added.
+
+        Returns:
+            tf.Tensor: A 1D tensor with trailing values replaced by padding tokens.
+        """
+        # Calculate the length of the padding part
+        padding_length = tf.shape(sequence)[0] - no_padding_end_index - 1
+        # Create a tensor of ones with the calculated padding length
+        ones_tensor = tf.ones(padding_length, dtype=tf.int32)
+        # Multiply the ones tensor by self.padding_id to create the padding_values tensor
+        padding_values = ones_tensor * self.padding_id
+
+        # Slice the input sequence up to no_padding_end_index + 1
+        sliced_sequence = sequence[: no_padding_end_index + 1]
+        # Concatenate the sliced sequence and padding_values to create the modified_sequence
+        modified_sequence = tf.concat([sliced_sequence, padding_values], axis=0)
+
+        # Return the modified_sequence with trailing values replaced by padding tokens
+        return modified_sequence
---
+++ tests/domain/dataset_preparation/test_batch_generator.py
@@ -0,0 +1,259 @@
+"""Tests for the Batch Generator used in the Transformer model."""
+from typing import List
+
+import pytest
+import tensorflow as tf
+
+from generative_music.domain.dataset_preparation.batch_gererator import \
+    BatchGenerator
+
+
+class TestBatchGenerator:
+    """A test class for the BatchGenerator.
+
+    The class tests if the input sequences are correctly preprocessed,
+    divided into subsequences, and organized into batches,
+    and if the generated batches have the correct shapes and data types.
+    """
+
+    def setup_method(self):
+        """Initialize the TestBatchGenerator tests.
+
+        Set up the data, batch size, sequence length, padding ID and start token ID.
+        This method is called before each test function is executed.
+        """
+        self.data = [[1, 2, 3, 1], [1, 4, 1, 5, 1, 6, 7, 1, 8], [1, 9, 10, 11, 12, 13]]
+        self.batch_size = 2
+        self.seq_length = 4
+        self.padding_id = 0
+        start_token_id = 1
+        self.vocab_size = 20
+        self.batch_generator = BatchGenerator(
+            self.data,
+            self.batch_size,
+            self.seq_length,
+            self.padding_id,
+            start_token_id,
+            self.vocab_size,
+        )
+
+    def test_generate_batches(self):
+        """Check the generated batch shapes and data types.
+
+        Tests if the generated batches have the correct shapes(batch_size, seq_length - 1)
+        and data types (tf.int32) after processing through the BatchGenerator.
+        """
+        dataset = self.batch_generator.generate_batches()
+        for batch in dataset.take(1):
+            src, tgt = batch
+            assert src.shape == (self.batch_size, self.seq_length - 1)
+            assert tgt.shape == (self.batch_size, self.seq_length - 1, self.vocab_size)
+            assert src.dtype == tf.int32
+            assert tgt.dtype == tf.int32
+
+    def test_process_sequences(self):
+        """Check the processed source and target sequences.
+
+        Tests if the source and target sequences are correctly processed
+        by removing the last token from the source and the first token from the target.
+        """
+        sequences = tf.constant([[1, 2, 3, 4]], dtype=tf.int32)
+        for eager_mode in [True, False]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            src, tgt = self.batch_generator._process_sequences(sequences)
+            # Convert sequences[:, 1:] to one-hot representation
+            tgt_expected = tf.one_hot(
+                sequences[:, 1:], depth=self.batch_generator.vocab_size, dtype=tf.int32
+            )
+            assert tf.reduce_all(tf.equal(src, sequences[:, :-1]))
+            assert tf.reduce_all(tf.equal(tgt, tgt_expected))
+
+    @pytest.mark.parametrize(
+        "sequence_index, expected_subsequences",
+        [
+            (0, [tf.constant([1, 2, 3, 1], dtype=tf.int32)]),
+            (
+                1,
+                [
+                    tf.constant([1, 4, 1, 5], dtype=tf.int32),
+                    tf.constant([1, 5, 0, 0], dtype=tf.int32),
+                    tf.constant([1, 6, 7, 0], dtype=tf.int32),
+                ],
+            ),
+            (2, [tf.constant([1, 9, 10, 11], dtype=tf.int32)]),
+        ],
+    )
+    def test_extract_subsequence(
+        self, sequence_index: int, expected_subsequences: List[tf.Tensor]
+    ):
+        """Check the extraction of subsequence.
+
+        Tests if the the function correctly extracts a subsequence
+        from the input sequence based on the start token index.
+
+        Args:
+            sequence_index (int): The index of the input sequence in the data.
+            expected_subsequences (List[tf.Tensor]):
+                A list of expected subsequences as tensors.
+        """
+        input_sequence = tf.constant(self.data[sequence_index], dtype=tf.int32)
+        for eager_mode in [True, False]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            extracted_subsequence = self.batch_generator._extract_subsequence(
+                input_sequence
+            )
+            # Check if extracted_subsequence matches any of the expected_subsequences
+            match_found = False
+            for expected_subsequence in expected_subsequences:
+                try:
+                    tf.debugging.assert_equal(
+                        extracted_subsequence, expected_subsequence
+                    )
+                    match_found = True
+                    break
+                except tf.errors.InvalidArgumentError:
+                    continue
+            assert match_found
+
+    def test_extract_subsequence_no_valid_start_token(self):
+        """Check the error handling when no valid start token is found.
+
+        Tests if the function raises an InvalidArgumentError
+        when there is no valid start token in the input sequence.
+        """
+        sequence = tf.constant([0, 2, 3, 4, 5], dtype=tf.int32)
+        for eager_mode in [True, False]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            with pytest.raises(
+                tf.errors.InvalidArgumentError,
+                match="No valid start token found in the sequence.",
+            ):
+                self.batch_generator._extract_subsequence(sequence)
+
+    @pytest.mark.parametrize(
+        "sequence",
+        [
+            tf.constant([1, 2, 3, 4], dtype=tf.int32),
+            tf.constant([1, 2, 3], dtype=tf.int32),
+        ],
+    )
+    def test_pad_sequence(self, sequence: tf.Tensor):
+        """Check the sequence padding.
+
+        Tests if the function correctly pads the input sequence
+        to the specified sequence length with padding tokens.
+
+        Args:
+            sequence (tf.Tensor): The input sequence as a tensor.
+        """
+        for eager_mode in [True, False]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            padded_sequence = self.batch_generator._pad_sequence(sequence)
+            assert tf.size(padded_sequence) == self.seq_length
+            if tf.size(sequence) >= self.seq_length:
+                tf.debugging.assert_equal(padded_sequence, sequence)
+            else:
+                # Check if the first part of the padded_sequence
+                # is the same as the input sequence
+                tf.debugging.assert_equal(
+                    padded_sequence[: tf.size(sequence)], sequence
+                )
+                # Check if the remaining part of the padded_sequence
+                # is filled with padding tokens
+                tf.debugging.assert_equal(
+                    padded_sequence[tf.size(sequence) :],
+                    tf.fill([self.seq_length - tf.size(sequence)], self.padding_id),
+                )
+
+    def test_find_actual_seq_length(self):
+        """Check the actual sequence length calculation.
+
+        Tests if the function correctly calculates
+        the actual sequence length by excluding padding tokens.
+        """
+        sequence = tf.constant([1, 2, 3, 0, 0], dtype=tf.int32)
+        for eager_mode in [True, False]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            actual_seq_length = self.batch_generator._find_actual_seq_length(sequence)
+            assert actual_seq_length == 3
+
+    def test_select_start_index(self):
+        """Check the start index selection.
+
+        Tests if the function correctly selects the start index
+        from the input start indices and actual sequence length.
+        """
+        start_indices = tf.constant([1], dtype=tf.int32)
+        actual_seq_length = tf.constant(4, dtype=tf.int32)
+        for eager_mode in [True, True]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            start_index = self.batch_generator._select_start_index(
+                start_indices, actual_seq_length
+            )
+            assert start_index == 1
+
+    @pytest.mark.parametrize(
+        "sequence, start_index, expected_end_index",
+        [
+            (
+                tf.constant([1, 2, 3, 4, 1, 5, 6, 7, 8], dtype=tf.int32),
+                tf.constant(0, dtype=tf.int32),
+                tf.constant(3, dtype=tf.int32),
+            ),
+            (
+                tf.constant([1, 2, 3, 4, 1, 5, 6, 1, 7, 8], dtype=tf.int32),
+                tf.constant(4, dtype=tf.int32),
+                tf.constant(6, dtype=tf.int32),
+            ),
+            (
+                tf.constant([1, 2, 3, 4, 1, 5, 6, 7, 8, 9], dtype=tf.int32),
+                tf.constant(4, dtype=tf.int32),
+                tf.constant(7, dtype=tf.int32),
+            ),
+        ],
+    )
+    def test_find_end_index(
+        self, sequence: tf.Tensor, start_index: tf.Tensor, expected_end_index: tf.Tensor
+    ):
+        """Check the end index calculation.
+
+        Tests if the the function correctly calculates the end index
+        based on the input sequence and start index.
+
+        Args:
+            sequence (tf.Tensor): The input sequence as a tensor.
+            start_index (tf.Tensor): The start index as a tensor.
+            expected_end_index (tf.Tensor): The expected end index as a tensor.
+        """
+        for eager_mode in [True, False]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            assert (
+                self.batch_generator._find_end_index(sequence, start_index)
+                == expected_end_index
+            )
+
+    def test_replace_trailing_values_with_padding(self):
+        """Check the replacement of trailing values with padding tokens.
+
+        Tests if the function correctly replaces values in the input sequence
+        after the specified index with padding tokens.
+        """
+        sequence = tf.constant([1, 2, 3, 4, 5], dtype=tf.int32)
+        no_padding_end_index = 2
+        expected_sequence = tf.constant([1, 2, 3, 0, 0], dtype=tf.int32)
+        for eager_mode in [True, False]:
+            # Enable/disable eager execution
+            tf.config.run_functions_eagerly(eager_mode)
+            modified_sequence = (
+                self.batch_generator._replace_trailing_values_with_padding(
+                    sequence, no_padding_end_index
+                )
+            )
+            assert tf.reduce_all(tf.equal(modified_sequence, expected_sequence))
