7ff9b5b941f70ed914a46e2a4cf7932ea32916b6
atsushieee <atsushi.tabata1204@gmail.com>
2023-08-11 08:59:54 +0900

    update: masked tokens exclusion from loss calculation

--- generative_music/domain/train/train_step/losses.py
+++ generative_music/domain/train/train_step/losses.py
@@ -10,7 +10,7 @@ class LabelSmoothedCategoricalCrossentropy(tf.keras.losses.Loss):
     """This class is a custom Keras loss function that implements label-smoothed crossentropy.
 
     It computes the crossentropy loss between the true labels and predicted probabilities,
-    with label smoothing applied to the true labels.
+    with label smoothing applied to the true labels and ignoring masked positions.
     Label smoothing is controlled by the `label_smoothing` argument, which is a float in [0, 1].
     When > 0, label values are smoothed, meaning the confidence on label values are relaxed.
     For example, if 0.1, use 0.1 / num_classes for non-target labels
@@ -19,17 +19,27 @@ class LabelSmoothedCategoricalCrossentropy(tf.keras.losses.Loss):
     which can help prevent overfitting and improve generalization.
     """
 
-    def __init__(self, label_smoothing: float = 0.1):
+    def __init__(self, masked_id: int, vocab_size: int, label_smoothing: float = 0.1):
         """Initialize the LabelSmoothedCategoricalCrossentropy class.
 
         Args:
-           label_smoothing (float, optional):
+            masked_id (int):
+                The value used for padding.
+                Loss will be ignored for positions with this value.
+            vocab_size (int):
+                The size of the number of unique words or tokens in the dataset.
+            label_smoothing (float, optional):
                The label smoothing factor to be applied to the true labels.
                Defaults to 0.1.
         """
         super().__init__()
+        self.masked_id = masked_id
+        self.vocab_size = vocab_size
+        # `reduction=tf.keras.losses.Reduction.NONE`: avoid reducing the loss to a scalar sum.
+        # Without this setting, masked parts would be included in the calculation,
+        # preventing the desired masking operation in the call function.
         self.smoothed_loss = tf.keras.losses.CategoricalCrossentropy(
-            label_smoothing=label_smoothing
+            label_smoothing=label_smoothing, reduction=tf.keras.losses.Reduction.NONE
         )
 
     def call(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
@@ -37,11 +47,19 @@ class LabelSmoothedCategoricalCrossentropy(tf.keras.losses.Loss):
 
         Args:
             y_true (tf.Tensor):
-                Ground truth tensor with shape (batch_size, seq_length, vocab_size).
+                Ground truth tensor with shape (batch_size, seq_length).
             y_pred (tf.Tensor):
                 Predicted probability tensor with shape (batch_size, seq_length, vocab_size).
 
         Returns:
             tf.Tensor: The label-smoothed categorical cross-entropy loss.
         """
-        return self.smoothed_loss(y_true, y_pred)
+        y_true_one_hot = tf.one_hot(y_true, depth=self.vocab_size, dtype=y_pred.dtype)
+        loss_values = self.smoothed_loss(y_true_one_hot, y_pred)
+        mask = tf.math.equal(y_true, self.masked_id)
+        mask = tf.cast(mask, dtype=loss_values.dtype)
+        loss_values *= 1 - mask
+        # The average loss by dividing the sum of loss_values
+        # by the sum of unmasked elements (1 - mask).
+        # This ensures that only the unmasked parts contribute to the final average loss.
+        return tf.reduce_sum(loss_values) / tf.reduce_sum(1 - mask)
--- tests/domain/train/train_step/test_losses.py
+++ tests/domain/train/train_step/test_losses.py
@@ -6,7 +6,7 @@ from generative_music.domain.train.train_step.losses import \
     LabelSmoothedCategoricalCrossentropy
 
 test_data = [
-    (0.0, 0.2899095),
+    (0.0, 0.28990924),
     (0.1, 0.4125352),
 ]
 
@@ -19,6 +19,15 @@ class TestLabelSmoothedCategoricalCrossentropy:
     It verifies the behavior of the label smoothing parameter
     and its effect on the loss value.
     """
+
+    def setup_method(self):
+        """Initialize the TestLabelSmoothedCategoricalCrossentropy tests.
+
+        Set up the true labels and predicted probabilities for testing the loss function.
+        This method is called before each test function is executed.
+        """
+        self.y_true = tf.constant([[1, 0]])
+        self.y_pred = tf.constant([[[0.1, 0.8, 0.1], [0.7, 0.2, 0.1]]])
 
     @pytest.mark.parametrize("label_smoothing, expected_loss", test_data)
     def test_loss(self, label_smoothing: float, expected_loss: float):
@@ -33,9 +42,22 @@ class TestLabelSmoothedCategoricalCrossentropy:
                 The expected loss value for the given true labels,
                 predicted probabilities and label smoothing.
         """
-        y_true = tf.constant([[[0.0, 1.0, 0.0], [1.0, 0.0, 0.0]]])
-        y_pred = tf.constant([[[0.1, 0.8, 0.1], [0.7, 0.2, 0.1]]])
+        loss = LabelSmoothedCategoricalCrossentropy(
+            vocab_size=3, masked_id=2, label_smoothing=label_smoothing
+        )
+        loss_value = loss(self.y_true, self.y_pred)
+        expected_value = tf.constant(expected_loss, dtype=tf.float32)
+        tf.debugging.assert_near(loss_value, expected_value, rtol=1e-8, atol=1e-8)
 
-        loss = LabelSmoothedCategoricalCrossentropy(label_smoothing=label_smoothing)
-        loss_value = loss(y_true, y_pred)
-        assert loss_value.numpy() == pytest.approx(expected_loss, abs=1e-6)
+    def test_loss_with_mask(self):
+        """Test if the computed loss value with mask matches the expected loss value.
+
+        This test checks if the loss function correctly applies the mask
+        to ignore padding tokens in the true labels during loss computation.
+        """
+        loss = LabelSmoothedCategoricalCrossentropy(
+            vocab_size=3, masked_id=0, label_smoothing=0.0
+        )
+        loss_value = loss(self.y_true, self.y_pred)
+        expected_value = tf.constant(0.22314353, dtype=tf.float32)
+        tf.debugging.assert_near(loss_value, expected_value, rtol=1e-8, atol=1e-8)
